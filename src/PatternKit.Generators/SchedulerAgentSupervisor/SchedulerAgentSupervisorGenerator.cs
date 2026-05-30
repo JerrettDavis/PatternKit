05621f5f7fded82d7aa43ffa8e435eb40c1a79db
JD Davis <jd@jerrettdavis.com>
2026-05-29 22:13:53 -0500

    test(generators): cover state transfer and scheduler (#443)

--- src/PatternKit.Generators/SchedulerAgentSupervisor/SchedulerAgentSupervisorGenerator.cs
+++ src/PatternKit.Generators/SchedulerAgentSupervisor/SchedulerAgentSupervisorGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
@@ -176,6 +177,60 @@ public sealed class SchedulerAgentSupervisorGenerator : IIncrementalGenerator
             sb.AppendLine();
         }
 
+        var containingTypes = GetContainingTypes(type);
+        var indentLevel = 0;
+        foreach (var containingType in containingTypes)
+        {
+            AppendTypeDeclaration(sb, containingType, indentLevel);
+            sb.AppendLine();
+            sb.AppendLine(new string(' ', indentLevel * 4) + "{");
+            indentLevel++;
+        }
+
+        AppendTypeDeclaration(sb, type, indentLevel);
+        sb.AppendLine();
+        var indent = new string(' ', indentLevel * 4);
+        sb.AppendLine(indent + "{");
+        var memberIndent = indent + "    ";
+        var bodyIndent = memberIndent + "    ";
+        sb.Append(memberIndent).Append("public static global::PatternKit.Cloud.SchedulerAgentSupervisor.SchedulerAgentSupervisor<").Append(workTypeName).Append(", ").Append(resultTypeName).Append("> ").Append(factoryMethodName).AppendLine("()");
+        sb.AppendLine(memberIndent + "{");
+        sb.Append(bodyIndent).Append("var policy = global::PatternKit.Cloud.SchedulerAgentSupervisor.SchedulerSupervisionPolicy<").Append(workTypeName).AppendLine(">.Create()");
+        sb.Append(bodyIndent).Append("    .MaxAttempts(").Append(maxAttempts).AppendLine(")");
+        sb.Append(bodyIndent).Append("    .RetryDelay(global::System.TimeSpan.FromMilliseconds(").Append(retryDelayMilliseconds).AppendLine("))");
+        if (retryName is not null)
+            sb.Append(bodyIndent).Append("    .RetryWhen(").Append(retryName).AppendLine(")");
+        sb.Append(bodyIndent).AppendLine("    .Build();");
+        sb.AppendLine();
+        sb.Append(bodyIndent).Append("return global::PatternKit.Cloud.SchedulerAgentSupervisor.SchedulerAgentSupervisor<").Append(workTypeName).Append(", ").Append(resultTypeName).Append(">.Create(\"").Append(Escape(supervisorName)).AppendLine("\")");
+        sb.Append(bodyIndent).AppendLine("    .Supervision(policy)");
+        foreach (var agent in agents)
+            sb.Append(bodyIndent).Append("    .Agent(\"").Append(Escape(agent.AgentName)).Append("\", ").Append(agent.MethodName).AppendLine(")");
+        sb.Append(bodyIndent).AppendLine("    .Build();");
+        sb.AppendLine(memberIndent + "}");
+        sb.AppendLine(indent + "}");
+        for (var i = containingTypes.Length - 1; i >= 0; i--)
+        {
+            sb.AppendLine(new string(' ', i * 4) + "}");
+        }
+
+        return sb.ToString();
+    }
+
+    private static INamedTypeSymbol[] GetContainingTypes(INamedTypeSymbol type)
+    {
+        var containingTypes = new Stack<INamedTypeSymbol>();
+        for (var current = type.ContainingType; current is not null; current = current.ContainingType)
+        {
+            containingTypes.Push(current);
+        }
+
+        return containingTypes.ToArray();
+    }
+
+    private static void AppendTypeDeclaration(StringBuilder sb, INamedTypeSymbol type, int indentLevel)
+    {
+        sb.Append(new string(' ', indentLevel * 4));
         sb.Append(GetAccessibility(type.DeclaredAccessibility)).Append(' ');
         if (type.IsStatic)
             sb.Append("static ");
@@ -183,25 +238,7 @@ public sealed class SchedulerAgentSupervisorGenerator : IIncrementalGenerator
             sb.Append("abstract ");
         else if (type.IsSealed && type.TypeKind == TypeKind.Class)
             sb.Append("sealed ");
-        sb.Append("partial ").Append(type.TypeKind == TypeKind.Struct ? "struct" : "class").Append(' ').Append(type.Name).AppendLine();
-        sb.AppendLine("{");
-        sb.Append("    public static global::PatternKit.Cloud.SchedulerAgentSupervisor.SchedulerAgentSupervisor<").Append(workTypeName).Append(", ").Append(resultTypeName).Append("> ").Append(factoryMethodName).AppendLine("()");
-        sb.AppendLine("    {");
-        sb.Append("        var policy = global::PatternKit.Cloud.SchedulerAgentSupervisor.SchedulerSupervisionPolicy<").Append(workTypeName).AppendLine(">.Create()");
-        sb.Append("            .MaxAttempts(").Append(maxAttempts).AppendLine(")");
-        sb.Append("            .RetryDelay(global::System.TimeSpan.FromMilliseconds(").Append(retryDelayMilliseconds).AppendLine("))");
-        if (retryName is not null)
-            sb.Append("            .RetryWhen(").Append(retryName).AppendLine(")");
-        sb.AppendLine("            .Build();");
-        sb.AppendLine();
-        sb.Append("        return global::PatternKit.Cloud.SchedulerAgentSupervisor.SchedulerAgentSupervisor<").Append(workTypeName).Append(", ").Append(resultTypeName).Append(">.Create(\"").Append(Escape(supervisorName)).AppendLine("\")");
-        sb.AppendLine("            .Supervision(policy)");
-        foreach (var agent in agents)
-            sb.Append("            .Agent(\"").Append(Escape(agent.AgentName)).Append("\", ").Append(agent.MethodName).AppendLine(")");
-        sb.AppendLine("            .Build();");
-        sb.AppendLine("    }");
-        sb.AppendLine("}");
-        return sb.ToString();
+        sb.Append("partial ").Append(type.TypeKind == TypeKind.Struct ? "struct" : "class").Append(' ').Append(type.Name);
     }
 
     private static int? GetNamedInt(AttributeData attribute, string name)
